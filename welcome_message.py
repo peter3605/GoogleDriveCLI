4da1b72d40e89c0d28ded4956616f8a3ecb222da
Peter Ho <peter.ho433@gmail.com>
2020-05-28 18:12:03 -0400

    Better welcome

--- welcome_message.py
+++ welcome_message.py
@@ -1,4 +1,11 @@
 def display():
-    print('-----------------------------------------')
-    print('| Welcome to the Google Drive CLI Tool! |')
-    print('-----------------------------------------')
+    f = open('welcome.txt', 'r')
+    for i in range(0,6):
+        line = f.readline().strip()
+        if i == 0 or i == 5:
+            line = " " + line
+        print(line)
+
+    print(' --------------------------')
+    print('| TO THE GOOGLE DRIVE CLI! |')
+    print(' --------------------------')
